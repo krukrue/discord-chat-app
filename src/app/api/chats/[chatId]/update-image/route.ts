743b742abbc1ad532f301b7170b6ad54442fbcce
bar0765 <Mrr7782@outlook.com>
2025-04-12 19:06:49 +0200

    Added group chat avatar update functionality, not tested

--- src/app/api/chats/[chatId]/update-image/route.ts
+++ src/app/api/chats/[chatId]/update-image/route.ts
@@ -1,30 +1,27 @@
-import { writeFile } from "fs/promises";
-import path from "path";
 import { db } from "@/server";
 import { chats } from "@/server/schema";
 import { eq } from "drizzle-orm";
 import { NextResponse } from "next/server";
+import { uploadFile } from "@/server/upload-file/upload-file";
 
 export async function POST(req: Request, { params }: { params: { chatId: string } }) {
     const formData = await req.formData();
-    const file = formData.get("image") as File;
+    const file = formData.get("image") as File | null;
 
-    if (!file) {
+    if (!file)
         return NextResponse.json({ error: "No file uploaded" }, { status: 400 });
+
+    try {
+        const fileBuffer = await file.arrayBuffer();
+        const buffer = Buffer.from(fileBuffer);
+        const fileUrl = await uploadFile(buffer, file.name);
+
+        await db.update(chats)
+            .set({ groupAvatar: fileUrl })
+            .where(eq(chats.id, params.chatId));
+
+        return NextResponse.json({ url: fileUrl });
+    } catch (e: any) {
+        return NextResponse.json({ error: "File upload failed: " + e.message }, { status: 500 });
     }
-
-    const bytes = await file.arrayBuffer();
-    const buffer = Buffer.from(bytes);
-
-    const fileName = `${Date.now()}-${file.name}`;
-    const filePath = path.join(process.cwd(), "public", "uploads", fileName);
-    const fileUrl = `/uploads/${fileName}`;
-
-    await writeFile(filePath, buffer);
-
-    await db.update(chats)
-        .set({ groupAvatar: fileUrl })
-        .where(eq(chats.id, params.chatId));
-
-    return NextResponse.json({ url: fileUrl });
 }
